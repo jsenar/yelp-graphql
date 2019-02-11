961a9c536976a8546d59665902ef4fb24aff1f3b
John <JLSenar@gail.com>
2019-02-10 17:47:25 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const request = require('request-promise');
+
+const baseUrl = 'https://api.yelp.com/v3/graphql';
+
+class YelpGraphQL {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  query(locale, {query, variables}) {
+
+    const body = {
+        query,
+        variables: variables ? JSON.stringify(variables) : undefined
+    };
+
+    return request({
+        method: 'POST',
+        uri: baseUrl,
+        headers: {
+            'Authorization': 'Bearer ' + this.apiKey,
+            'Content-Type': 'application/json', 
+            'Accept-Language': locale
+        },
+        body,
+        json: true
+    }).then((response) => {
+        return response;
+    }).catch((err) => {
+        return err;
+    });
+  }
+}
+
+module.exports = YelpGraphQL;
